4ecd209c0a7761a2fa111f2e21ca02887903bb7a
ysnnis <yannis_123@live.com>
2018-01-29 20:25:40 +0800

    add project

---
+++ MyProject.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MyProject.Web.Controllers
+{
+    public class HomeController : MyProjectControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# MyAbpProject
-ABP Demo
